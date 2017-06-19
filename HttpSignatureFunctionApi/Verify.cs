64e4f4b2ed28ecfc68a29bbfbb9f6b9cb4f32568
kadodds <kadodds@microsoft.com>
2017-06-19 09:43:52 -0700

    Updating repo

--- HttpSignatureFunctionApi/Verify.cs
+++ HttpSignatureFunctionApi/Verify.cs
@@ -5,38 +5,73 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using System.Net.Http.Headers;
+using http.signature;
+using System;
+using System.Collections.Generic;
 
 namespace HttpSignatureFunctionApi
 {
     public static class Verify
     {
+        private static TraceWriter logger;
+
         [FunctionName("HttpTriggerCSharp")]
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
+            logger = log;
+            logger.Info("C# HTTP trigger function processed a request.");
 
             // Step 1) Check that HTTPRequest Message contains Authorization header 
-                // auth-scheme is 'Signature'
-                // auth-param includes keyId algorithm headers signature (all required in this case)
-
+            if (req.Headers.Authorization == null || !Signature.IsValidSignature(req.Headers.Authorization.Scheme, req.Headers.Authorization.Parameter))
+            {
                 // send 401 Unauthorized if Request does not contain necessary headers + info
-                    // specify which headers are expected in WW-Authenticate header 
-             
+                // specify which headers are expected in WW-Authenticate header 
+                return Send401Response("Authorization Attempt Failed, Invalid Signature");
+            }
+
             // Step 2) Verify Signature 
 
-                // a) Parse HttpResponseMessage to generate a Request object 
+            // a) Parse HttpResponseMessage to generate a Request object 
+            Request request = Parser.ParseRequestFromHttpRequestMessage(req);
 
-                // b) Parse Authorization header  
-                    // keyId
-                    // algorithm
-                    // Signature (encoded value) 
+            // b) Parse Authorization header 
+            Dictionary<string, List<string>> parsedAuthenticationHeaders = Parser.ParseAuthorizationHeader(req.Headers.Authorization);
+            // keyId
+            string keyId = parsedAuthenticationHeaders["keyId"][0];
+            // algorithm
+            string algorithm = parsedAuthenticationHeaders["algorithm"][0];
+            // Signature (encoded value) 
+            string requestSignature = parsedAuthenticationHeaders["signature"][0];
+            // all the headers used to sign the original signature 
+            List<string> origHeaders = parsedAuthenticationHeaders["headers"];
 
-                // c) Create new Signature object with keyId, algorithm and Request object
-                   
-                // d) Create new Signer object with Signature object
+            // c) Create new Signature object with keyId, algorithm and Request object
+            Signature signature = new Signature(keyId, algorithm, request);
 
-                // e) Call signer.Verify() given the encoded signature you received in the original HTTP request 
-                
+            // d) Create new Signer object with Signature object
+            Signer signer = new Signer(signature);
+
+            // e) Call signer.Verify() given the encoded signature you received in the original HTTP request 
+            if (signer.Verify(requestSignature))
+            {
+                // if true, signatures match, send back 200 OK
+                return req.CreateResponse(HttpStatusCode.OK, "Authorization Attempt Successful");
+            }
+            else
+            {
+                // if false, signatures did not match, send back error (401?)
+                return Send401Response("Authorization Attempt Failed, Signature Verification Failed");
+            } 
+            
+        }
+        
+        private static HttpResponseMessage Send401Response(string message)
+        {
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            response.ReasonPhrase = message;
+            response.Headers.WwwAuthenticate.Add(new AuthenticationHeaderValue("Signature", "headers=\"(request-target) date digest\""));
+            return response;
         }
     }
 }
