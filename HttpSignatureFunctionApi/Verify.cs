db5e7dcfa2089204b8460bf1b705b3196c157415
kadodds <kadodds@microsoft.com>
2017-06-22 11:42:31 -0700

    Added more log statements in function

--- HttpSignatureFunctionApi/Verify.cs
+++ HttpSignatureFunctionApi/Verify.cs
@@ -15,17 +15,19 @@ namespace HttpSignatureFunctionApi
 {
     public static class Verify
     {
+        public static TraceWriter logger; 
 
         [FunctionName("HttpSignatureVerificationWorker")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "HttpSignature/verify")]HttpRequestMessage req, TraceWriter log)
         {
-            // add route**
-            log.Info("HttpSignatureVerificaion processed request");
+            logger = log;
+            
+            logger.Info("HttpSignatureVerificaion processed request");
 
             // Step 1) Check that HTTPRequest Message contains Authorization header 
             if (req.Headers.Authorization == null)
             {
-                log.Info("Request object did not contain an Authorization Header");
+                logger.Info("Request object did not contain an Authorization Header");
                 return Send401Response("Authorization Attempt Failed, No Authorization Header");
             }
 
@@ -33,40 +35,40 @@ namespace HttpSignatureFunctionApi
             {
                 // Step 2) Verify Signature 
                 Signature signature = Signature.FromHttpRequest(req);
-                log.Info("Created signature from Http request");
+                logger.Info("Created signature from Http request");
                 string originalRequestSignature = signature.EncodedSignature;
-                log.Info($"Request signature passed into function: {originalRequestSignature}");
+                logger.Info($"Request signature passed into function: {originalRequestSignature}");
 
                 // d) Create new Signer object with Signature object
                 Signer signer = new Signer(signature);
-                log.Info("Created signer instance with signature");
+                logger.Info("Created signer instance with signature");
 
                 // e) Call signer.Verify() given the encoded signature you received in the original HTTP request 
-                log.Info("Calling verify on signer");
+                logger.Info("Calling verify on signer");
                 if (signer.Verify(originalRequestSignature))
                 {
-                    log.Info("Signature verification passed.");
+                    logger.Info($"Signature verification passed: {signature.EncodedSignature}");
                     return Send200Response("Signature Verification Succesfull");
                 }
                 else
                 {
-                    log.Info("Signature verification failed.");
+                    logger.Info($"Signature verification failed: {signature.EncodedSignature}");
                     return Send401Response("Authorization Attempt Failed, Signature Verification Failed");
                 }
             }
             catch (InvalidSignatureString ex)
             {
-                log.Error("Invalid Signature String", ex);
+                logger.Error("Invalid Signature String", ex);
                 return Send401Response("Signature string could not be used, was invalid");
             }
             catch (InvalidAuthorizationHeader ex)
             {
-                log.Error("Invalid Authorization Header", ex);
+                logger.Error("Invalid Authorization Header", ex);
                 return Send401Response("Invalid Authorization Header");
             }
             catch (Exception ex)
             {
-                log.Error("Error Occured", ex);
+                logger.Error("Error Occured", ex);
                 return Send401Response("Error Occured");
             }
             
@@ -81,9 +83,13 @@ namespace HttpSignatureFunctionApi
 
             // indicate that varification failed in response content (as JSON object w/ value 'verified')
             response.Content = new StringContent("{\"verified\":\"false\"}", System.Text.Encoding.UTF8, "application/json");
+            logger.Info("Setting response content - {verified: false}");
 
             // specify which headers are expected in WWW-Authenticate header 
             response.Headers.WwwAuthenticate.Add(new AuthenticationHeaderValue("Signature", "headers=\"date digest\""));
+            logger.Info("Setting WWW-Authenticate header");
+
+            logger.Info("Sending 401 Response");
             return response;
         }
 
@@ -91,9 +97,12 @@ namespace HttpSignatureFunctionApi
         {
             HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
             response.ReasonPhrase = message;
+
             // indicate that verification has been successful in response content (as JSON object w/ value 'verified')
             response.Content = new StringContent("{\"verified\":\"true\"}", System.Text.Encoding.UTF8, "application/json");
+            logger.Info("Setting response content - {verified: true}");
 
+            logger.Info("Sending 200 Response");
             return response;
         }
     }
